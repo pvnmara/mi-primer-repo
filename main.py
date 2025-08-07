3f2a0ee5fd51243548c725a2af0bc9243be5c2bf
Mara <pavonmara578@mail.com>
2025-08-07 19:47:46 +0200

    Juego base

---
+++ main.py
@@ -0,0 +1,13 @@
+numero_secreto = 4
+
+
+print("\nEscribi un numero que sea de 1 a 20!\n")
+
+respuesta = input("intento: ")
+
+respuesta = int(respuesta)
+
+if numero_secreto == respuesta: 
+    print("\Respuesta correcta!")
+else: 
+    print("\nIncorrecto! ")
